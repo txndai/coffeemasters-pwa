e6dea8447501717bdaf67ef2e53c1be347cfa5f5
Guide Fari <guide.fari@flexperto.com>
2023-03-05 18:19:28 +0200

    feat: add persistent storage

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,17 @@ import Menu from './Menu.js';
 import Order from './Order.js';
 import Router from './Router.js';
 
+(async function() {
+    if (navigator.storage && navigator.storage.persist){
+        if (!await navigator.storage.persisted()) {
+            const result = await navigator.storage.persist()
+            // in chrome, if this result is false, there's nothing you can do
+            // firefox however, explicitly asks you for permission
+            console.log('result:', result)
+        }
+    }
+})()
+
 window.addEventListener("DOMContentLoaded", () => {
     Router.init();
     Menu.load();
